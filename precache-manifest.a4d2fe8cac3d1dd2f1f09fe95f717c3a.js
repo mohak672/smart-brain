9680cda736d892001796795ba84e55cf5be8127a
Mohak Chaudhari <mohak.672@gmail.com>
2020-04-25 19:04:38 +0530

    Updates

---
+++ precache-manifest.a4d2fe8cac3d1dd2f1f09fe95f717c3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f8aa10d57217901f0a76c83557df91d",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "c1827c15cde97917362a",
+    "url": "/smart-brain/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "88771393fbc35833c319",
+    "url": "/smart-brain/static/css/main.23e65fa5.chunk.css"
+  },
+  {
+    "revision": "c1827c15cde97917362a",
+    "url": "/smart-brain/static/js/2.46e0c8ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smart-brain/static/js/2.46e0c8ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88771393fbc35833c319",
+    "url": "/smart-brain/static/js/main.1af8522d.chunk.js"
+  },
+  {
+    "revision": "201e25242bafab5f0345",
+    "url": "/smart-brain/static/js/runtime-main.332ba265.js"
+  }
+]);
